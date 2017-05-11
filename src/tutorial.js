70229738bd01a7686a2ddcb6e23709780db680e6
Raoul Baron <raoul.baron@student.hpi.de>
2017-05-11 17:20:59 +0200

    rename class names

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,8 +1,8 @@
 /* @flow */
 
 (function(window, document, undefined) {
-    class KB {
-        constructor({identifier = "kb", debug = true} = {}) {
+    class Tutorial {
+        constructor({identifier = "tutorial-wrapper", debug = true} = {}) {
             this.identifier = identifier;
             this.debug      = debug;
             this.step       = 0;
@@ -12,12 +12,12 @@
             if(this.elems.length === 0) {
                 throw new Error("No activitys point defined");
             }
-            else if(!this.elems.every(el => el.getAttribute("kb-step"))) {
+            else if(!this.elems.every(el => el.getAttribute("t-step"))) {
                 throw new Error("Not all steps defined");
             }
             else {
                 this.elems.sort((a, b) => {
-                    return parseInt(a.getAttribute("kb-step")) - parseInt(b.getAttribute("kb-step"));
+                    return parseInt(a.getAttribute("t-step")) - parseInt(b.getAttribute("t-step"));
                 });
 
                 console.log(this.elems)
@@ -33,7 +33,7 @@
         }
     }
 
-    if(!window.KB) {
-        window.KB = KB;
+    if(!window.Tutorial) {
+        window.Tutorial = Tutorial;
     }
 })(window, document)
