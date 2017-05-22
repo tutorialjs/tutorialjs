30bcd87ca5cce355e5d233d483e78b4f90fc2769
Usame Algan <usame33@hotmail.de>
2017-05-22 02:21:59 +0200

    created demo

--- demo/test/main.js
+++ demo/test/main.js
@@ -1,20 +1,23 @@
-var kb = new Tutorial({
-    steps: [
-        {
-            highlight: ".a",
-            text: "now we also show text"
-        },
-        {
-            highlight: ".b",
-            text: "mom get the camera"
-        },
-        {
-            highlight: ".c",
-            text: "star me on github"
-        }],
-    debug: true,
-    name: "nico<3",
-    persistent: false,
-    buttons: {close: "Schließen"},
-    autoplay: true
-});
+window.onload = function() {
+    var kb = new Tutorial({
+        steps: [
+            {
+                highlight: ".first-step",
+                text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+            },
+            {
+                highlight: ".second-step",
+                text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+            },
+            {
+                highlight: ".third-step",
+                text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+            }],
+        debug: true,
+        name: "nico<3",
+        persistent: false,
+        buttons: {close: "Schließen"},
+        autoplay: true
+    });
+};
+
