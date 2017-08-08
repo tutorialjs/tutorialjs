a2410eff457ad597c9d84a601fa8ca89f26346fe
Usame Algan <usame33@hotmail.de>
2017-08-08 02:12:35 +0200

    small fix for the demo to work

--- dist/tutorial.js
+++ dist/tutorial.js
@@ -220,12 +220,9 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
                     elems.sort(function (a, b) {
                         return parseInt(a.getAttribute("t-step")) - parseInt(b.getAttribute("t-step"));
                     });
-                    /* let position = item.getAttribute("t-position") || "auto";
-                    this.elems = elems.map(item => new NormalStep(this, item, item.getAttribute("t-text"), position, {
-                        title: item.getAttribute("t-title")
-                    })); */
+                    var position = item.getAttribute("t-position") || "auto";
                     this.elems = elems.map(function (item) {
-                        return new NormalStep(_this3, item, item.getAttribute("t-text"), {
+                        return new NormalStep(_this3, item, item.getAttribute("t-text"), position, {
                             title: item.getAttribute("t-title")
                         });
                     });
