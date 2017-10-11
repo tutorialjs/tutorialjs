b36375cfa88d7f37bee041690db69890194efc8a
Usame Algan <usame33@hotmail.de>
2017-10-11 17:29:47 +0200

    added schulcloud files

---
+++ demo-schulcloud/main.js
@@ -0,0 +1,30 @@
+tutorial = new Tutorial("schul-cloud-onboarding", {
+    steps: [
+        {
+            highlight: ".sc-branding",
+            text: "Hier finden Sie die wichtigsten Anwendungsbereiche",
+			position:"right",
+        },
+		{
+			highlight: ".topbar .dropdowns",
+			text: "Hier finden Sie übergreifende Funktionen"
+		},
+		{
+			highlight: ".qr-code-action",
+			text: "Ganz einfach QR-Codes erstellen"
+		},
+		{
+			highlight: ".notification-dropdown",
+			text: "Benachrichtigungen erhalten"
+		},
+		{
+			highlight: ".help-drodown",
+			text: "Hilfebereich bei für Fragen und Probleme"
+		},
+		{
+			highlight: ".topbar .dropdowns > li:last-child",
+			text: "Einstellungen vornehmen und Log-out"
+		}],
+	autoplay: true
+});
+
